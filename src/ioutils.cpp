b71705a8c7c760257d9397776d82f1eb792ca303
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2019-04-06 22:13:10 +0100

    Implement IO utils

--- src/ioutils.cpp
+++ src/ioutils.cpp
@@ -1,11 +1,30 @@
 #include "ioutils.hpp"
+#include <fstream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 namespace sivox {
     std::string read_text_file(std::string const& path) {
-        return "";
+        if (fs::exists(path) && fs::is_regular_file(path)) {
+            std::ifstream file(path);
+
+            file.seekg(0, file.end);
+            auto length = file.tellg();
+            file.seekg(0, file.beg);
+
+            std::string text(length, '\0');
+            file.read(&text[0], length);
+
+            return text;
+        }
+        else {
+            return "";
+        }
     }
 
     void write_text_file(std::string const& path, std::string const& contents) {
-
+        std::ofstream file(path);
+        file << contents;
     }
 }
